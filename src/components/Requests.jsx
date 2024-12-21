d2a6685f4f081fd2d7c3c2624b6f69bbafa478dd
tanishq <tanishqjain23112001@gmail.com>
2024-12-22 02:18:53 +0530

    build a removeRequests feature from redux

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,23 +2,40 @@ import axios from 'axios'
 import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
-import { addRequest } from '../utils/requestSlice'
+import { addRequest  } from '../utils/requestSlice'
+import {removeRequest} from '../utils/requestSlice'
 
 export const Requests = () => {
 
   const dispatch = useDispatch();
   const requests = useSelector((store)=>store.requests);
 
+  
   const fetchRequests = async()=>{
     try{
         const res = await axios.get(BASE_URL + "/user/requests" , {withCredentials: true});
         console.log(res.data.data);
         dispatch(addRequest(res?.data?.data));
-    }
-    catch(err){
+
 
     }
+    catch(err){
+        console.error(err);
+    }
   }
+
+
+  const reviewRequest = async(status, _id)=>{
+    try{
+       const res = await axios.post(BASE_URL + "/request/review/" + status + "/" + _id , {} , {withCredentials:true});
+
+       dispatch(removeRequest(_id));
+    }
+    catch(err){
+       console.log(err);
+    }
+  }
+
 
   useEffect(()=>{
     fetchRequests();
@@ -58,8 +75,8 @@ export const Requests = () => {
                         </div>
 
                         <div className='my-5'>
-                        <button type="button" className="mx-2 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Reject</button>
-                        <button type="button" className="mx-2 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Accept</button>
+                        <button type="button" onClick={()=>reviewRequest("rejected", request._id)}  className="mx-2 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Reject</button>
+                        <button type="button" onClick={()=>reviewRequest("accepted", request._id)}  className="mx-2 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Accept</button>
                         </div>
                    
                 </div>
--- src/utils/requestSlice.jsx
+++ src/utils/requestSlice.jsx
@@ -8,7 +8,8 @@ const requestSlice = createSlice({
             return action.payload;
         },
         removeRequest: (state,action)=>{
-            return null;
+            const newArray = state.filter((x)=> x._id !== action.payload);
+            return newArray;
         }
     }
 })
