13daa5614ee7d235d4e9b11cd433f92166ae2804
Tanishq Jain <tanishq@gmail.com>
2026-02-22 16:54:53 +0530

    Project cleanup and folder restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
             <Route path="/"  element={<Body/>} >
                 <Route path="/" element={<Feed/>}/>
                 <Route path="/login" element={<Login/>}/>
+                {/* <Route path="/signup" element={<Login/>}/> */}
                 <Route path="/profile" element={<Profile/>}/>
                 <Route path="/connections" element={<Connections/>}/>
                 <Route path="/requests" element={<Requests/>}/>
