873b852c7afb3bf00fc840b7d41d57e6e8a7d97c
Tanishq Jain <tanishq@gmail.com>
2026-03-26 17:29:58 +0530

    frontend changes

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://13.62.17.135:3000";
+export const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "http://13.62.17.135:3000";
